206d6c6c3d67f123adc5f18de3d1916733f8ed44
Tshegofatso Mkhabela <161270475+TshegofatsoMkhabela@users.noreply.github.com>
2025-03-12 09:21:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -254,7 +254,7 @@ Do I need a team for the hackathon?
 You can form a team before registration or be assigned to one randomly.
 `;
 
-const API_KEY = "AIzaSyAI5vTm1E2qR0CdJfG3A6syn85q_chiSvE";
+const API_KEY = "none";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-pro",
